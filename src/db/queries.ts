0d3206446f05e6fffd51229a31d1722c76eb37ad
Suprada-2002 <supradatripathy789@gmail.com>
2025-11-12 08:57:18 +0530

    queries

---
+++ src/db/queries.ts
@@ -0,0 +1,30 @@
+import type { UUID} from 'crypto'
+import { db } from './db'
+import { todosTable, usersTable } from './schema'
+import { desc, eq } from 'drizzle-orm'
+
+type NewTodo = {
+    userId: UUID;
+    title: string;
+    description?: string;
+    completed?: boolean;
+}
+
+export const createUser = async (email: string, password: string) => {
+    const passwordHash = await Bun.password.hash(password);
+    const [createdUser] = await db.insert(usersTable).values({
+        email, passwordHash
+    }).returning();
+    return createdUser.id as UUID;
+}
+
+export const getTodosByUserId = async (userId: UUID) => {
+  const [todos] = await db.select().from(todosTable).where(eq(todosTable.id, userId)).orderBy(desc(todosTable.createdAt));
+   return todos;
+}
+
+export const createTodos = async (todo: NewTodo) => {
+    const [createdTodo] = await db.insert(todosTable).values(todo).returning();
+    return createdTodo;
+}
+
